259ab972f52b392972ec8aa851afe0a47aba61b8
shidalgoBP <barcecheo2319@gmail.com>
2021-06-03 09:43:28 -0500

    static method order

--- src/main/ChapterExercise.java
+++ src/main/ChapterExercise.java
@@ -28,26 +28,9 @@ public class ChapterExercise {
             Vault vault = new Vault(nbrCharacters, nbrDigits);
             robbers.get(0).addVault(vault);
 
-            order(robbers);
+            Robber.order(robbers);
         }
 
         System.out.println(robbers.get(robbers.size() - 1).timeOfWork());
-    }
-
-    private static void order(List<Robber> list) {
-        Robber temp;
-        boolean sorted = false;
-
-        while (!sorted) {
-            sorted = true;
-            for (int i = 0; i < list.size()-1; i++) {
-                if (list.get(i).compareTo(list.get(i + 1)) > 0) {
-                    temp = list.get(i);
-                    list.set(i, list.get(i + 1));
-                    list.set(i + 1, temp);
-                    sorted = false;
-                }
-            }
-        }
     }
 }
--- src/main/model/Robber.java
+++ src/main/model/Robber.java
@@ -7,11 +7,28 @@ public class Robber implements Comparable<Robber> {
 
     private List<Vault> vaults;
 
-    public Robber(){
+    public Robber() {
         this.vaults = new ArrayList<>();
     }
 
-    public void addVault(Vault vault){
+    public static void order(List<Robber> list) {
+        Robber temp;
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                    temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                    sorted = false;
+                }
+            }
+        }
+    }
+
+    public void addVault(Vault vault) {
         vaults.add(vault);
     }
 
