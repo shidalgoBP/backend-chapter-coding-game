8542f160a5fd0c0353c817813e6c61a2a20f57b8
shidalgoBP <barcecheo2319@gmail.com>
2021-06-03 10:03:09 -0500

    Add Lombock library

--- src/main/model/Vault.java
+++ src/main/model/Vault.java
@@ -1,17 +1,12 @@
 package main.model;
 
+import lombok.Getter;
+
+@Getter
 public class Vault {
-    private int nbrCombinations;
+    private final int nbrCombinations;
 
     public Vault(int nbrCharacters, int nbrDigits){
         this.nbrCombinations = (int) (Math.pow(10, nbrDigits) * Math.pow(5, nbrCharacters - nbrDigits));
-    }
-
-    public Vault(int nbrCombinations){
-        this.nbrCombinations = nbrCombinations;
-    }
-
-    public int getNbrCombinations() {
-        return nbrCombinations;
     }
 }
